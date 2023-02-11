b01429b508168620d0ac204795039404a7f60428
Calupator <calupator@mail.ru>
2023-02-11 14:28:50 +0300

    Решение для комента к DZ-4

--- DZ-4/my.sql
+++ DZ-4/my.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `students` (
+﻿CREATE TABLE `students` (
   `id` int(10) NOT NULL COMMENT 'id пользователя',
   `name` text COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Имя',
   `age` date NOT NULL COMMENT 'Дата рождения',
@@ -13,9 +13,18 @@ INSERT INTO `students` (`id`, `name`, `age`, `address`) VALUES
 (5, 'Женька', '1983-03-21', 'Москва'),
 (6, 'Рыжий', '1998-08-16', 'Москв'),
 (7, 'Колобок', '2000-01-15', 'на чердаке'),
-(8, 'Эдик', '2004-06-01', 'Москва');
+(8, 'Эдик', '2004-06-01', 'Москва'),
+(9, 'Пельмень', '2005-02-11', 'нИ_тАМА');
+/*(9, 'Пельмень', '2005-02-13', 'нИ_тАМА');*/
 
-SELECT * FROM students WHERE 
+/*SELECT * FROM students WHERE 
 address = 'Москва' AND
-age >= '1993-01-01' AND 
-age < '2004-12-31';
+age >= '1993-01-01' AND
+age < '2004-12-31';*/
+
+UPDATE students SET age = '2005-02-12' WHERE name = 'Пельмень';
+
+SELECT * FROM students WHERE
+age <= DATE_SUB(NOW(), INTERVAL 18 YEAR) AND
+age > DATE_SUB(NOW(), INTERVAL 30-1 YEAR)
+ORDER BY age;
